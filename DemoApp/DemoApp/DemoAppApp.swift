29cf0928f8634d0554fc1631cc37e20e1d4a85c6
Naru <naru@naruaway.com>
2024-07-15 18:25:23 +0900

    Create "DemoApp" from "Create New Project" in Xcode 15.3

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Naru on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
